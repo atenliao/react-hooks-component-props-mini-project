0cb0781e701adc5d355a19456d2dad420e856417
Zhongming <atenliao@gmail.com>
2023-05-27 23:39:36 -0700

    update Article.js

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,31 +1,31 @@
 import React from "react";
 
 function Article({title, date="January 1, 1970", preview, minutes}){
-    let coffee = []
-    let bento =[]
-    if(minutes < 30){
-        for(let i=0;i< Math.round(minutes/5);i++){
-            coffee.push("☕️")
-        }
+    // let coffee = []
+    // let bento =[]
+    // if(minutes < 30){
+    //     for(let i=0;i< Math.round(minutes/5);i++){
+    //         coffee.push("☕️")
+    //     }
         
-    }else{
-        for(let i=0;i< Math.round(minutes/10);i++){
-            bento.push("🍱")
-        }
-    }
+    // }else{
+    //     for(let i=0;i< Math.round(minutes/10);i++){
+    //         bento.push("🍱")
+    //     }
+    // }
     // console.log(coffee)
     return (
         <article>
             <h3>{title}</h3>
             <small>{date} • &nbsp;
-            {
+            {/* {
             minutes < 30 ? 
             // 
             coffee.map(item=>item)
             :
             bento.map(item=>item)
             }
-            &nbsp;
+            &nbsp; */}
             {minutes}
             &nbsp; min read
             </small>
